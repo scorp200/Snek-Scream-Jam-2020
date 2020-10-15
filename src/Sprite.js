770d396b9c58e245bd346225fc32aafd8b014c1b
JackOatley <jack.f.oatley@gmail.com>
2020-10-15 15:22:00 +0100

    Sprite update.

--- src/Sprite.js
+++ src/Sprite.js
@@ -1,13 +1,20 @@
 import * as EMTI from './lib/emti.js';
 
-const template = new EMTI.PlaneGeometry(8, 8);
+const TEXTURE_WIDTH = 8;
+const TEXTURE_HEIGHT = 8;
+
+const template = new EMTI.PlaneGeometry(1, 1);
 const texture = new EMTI.Texture("img/spritesheet.png", { noMipmaps: true });
 const material = new EMTI.TextureMaterial(texture);
 
 export class Sprite extends EMTI.Mesh {
 
-	constructor(x, y) {
-		const plane = template.clone().shiftCoords(x, y);
+	constructor(x, y, width, height) {
+		const plane = template.
+			clone()
+			.scale(width, height, 1)
+			.scaleCoords(width / TEXTURE_WIDTH, height / TEXTURE_HEIGHT)
+			.shiftCoords(x / TEXTURE_WIDTH, y / TEXTURE_HEIGHT);
 		super(plane, material);
 	}
 
